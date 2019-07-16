90d402576680675330b71c37512f5e53968cc629
Laura Beatris <laurabeatriserafim@gmail.com>
2019-07-15 21:29:23 -0300

    Updates

---
+++ precache-manifest.cd3b1e3157fea3f33acbadb5cc522aef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653b59b7701fdbed4aebad94e1e9a095",
+    "url": "/2budget/index.html"
+  },
+  {
+    "revision": "2f99cc6087e271a87e12",
+    "url": "/2budget/static/css/2.e15833e6.chunk.css"
+  },
+  {
+    "revision": "10c8f7f2317c29026719",
+    "url": "/2budget/static/css/main.7b808305.chunk.css"
+  },
+  {
+    "revision": "2f99cc6087e271a87e12",
+    "url": "/2budget/static/js/2.d626c0f4.chunk.js"
+  },
+  {
+    "revision": "10c8f7f2317c29026719",
+    "url": "/2budget/static/js/main.8e65b0a0.chunk.js"
+  },
+  {
+    "revision": "c736112abc0e990cb17b",
+    "url": "/2budget/static/js/runtime~main.3a6e1513.js"
+  },
+  {
+    "revision": "4ad29cbf3f011842116065335b27f983",
+    "url": "/2budget/static/media/budget-illustration.4ad29cbf.png"
+  },
+  {
+    "revision": "1b3f94dcc93d39b34c944adc8e3b7567",
+    "url": "/2budget/static/media/lock.1b3f94dc.svg"
+  },
+  {
+    "revision": "5e946c20186e33b608fbb3388fff6ff5",
+    "url": "/2budget/static/media/user.5e946c20.svg"
+  }
+]);
