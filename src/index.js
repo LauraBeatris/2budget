378ec936c2744003c0d579073c2fbbca991e97dd
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-17 15:20:44 -0300

    testing deploy

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 
 const store = configureStore();
-
-if (process.env.NODE_ENV === "test") {
-  // For default it looks for a .env file so we need to set up others with a
-  // path
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
-}
+require("dotenv").config({ path: ".env.development" });
 
 // Providing the store to all components of our app
 const app = (
