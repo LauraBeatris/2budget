437e4c61ce2a3bf167b9fcee9a8259025d7eee4a
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-17 13:22:18 -0300

    conditional to .env files

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import AppRouter from "./routers/AppRouter";
 
 const store = configureStore();
 
+if (process.env.NODE_ENV === "test") {
+  // For default it looks for a .env file so we need to set up others with a
+  // path
+  require("dotenv").config({ path: ".env.teste" });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
+
 // Providing the store to all components of our app
 const app = (
   <Provider store={store}>
