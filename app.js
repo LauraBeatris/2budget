771132e6a53407d53f62771e8108d192d05a14a4
Laura Beatris <laurabeatriserafim@gmail.com>
2019-08-17 15:55:17 -0300

    fixing deploy

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+require("dotenv").config({ path: ".env.development" });
 
 let app = express();
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || "8080";
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.set('port', port);
+app.set("port", port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
