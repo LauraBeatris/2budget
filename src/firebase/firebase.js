f0564e1ab7ebac4b16f85980bee1fe5be3c2761e
LauraBeatris <laurabeatriserafim@gmail.com>
2020-01-11 21:16:56 -0300

    fix - redirect config to prevent heroku 404 error

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from "firebase";
 
-// Your web app's Firebase configuration
+// Your web app Firebase configuration
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -10,6 +10,7 @@ var firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
